fe45b85fa046c57c26cb30fa79d2aa3d1c563490
Jennifer Sung <sung.jenni93@gmail.com>
2023-05-02 21:30:06 -0400

    Added console.log messages

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// introduction 
+// Introduction 
 let userName = prompt('what is your name?');
 
 alert(`Welcome to my site ${userName} ! How about we play a guessing game. Please answer with a yes/no or y/n`);
@@ -9,20 +9,25 @@ alert(`Welcome to my site ${userName} ! How about we play a guessing game. Pleas
 let questionOne = prompt('First Question. Did I got to college?').toLocaleLowerCase();
 
 if(questionOne === 'y' || questionOne === 'yes') {
+  //console.log(`Correct ${userName}! I did. I recently graduated.`);
   alert(`Correct ${userName}! I did. I recently graduated.`);
 } else if (questionOne === 'n' || questionOne === 'no') {
+  //console.log(`Sorry ${userName} that is not correct, I have a college degree.`);
   alert(`Sorry ${userName} that is not correct, I have a college degree.`);
 }
 
 // console.log(questionOne);
 
+
 //Second Question
-let questionTwo = prompt('Second Question. Am I located in the eastcoast?').toLocaleLowerCase();
+let questionTwo = prompt('Second Question. Am I located in the east coast?').toLocaleLowerCase();
 
 if(questionTwo === 'y' || questionTwo === 'yes') {
+  //console.log(`Great guess ${userName}! Yes, I live in NYC.`);
   alert(`Great guess ${userName}! Yes, I live in NYC.`);
 } else if (questionTwo === 'n' || questionTwo === 'no') {
-  alert(`Sorry ${userName} that is not correct, I live in the eastcoast.`);
+  //console.log(`Sorry ${userName} that is not correct, I live in the east coast.`)
+  alert(`Sorry ${userName} that is not correct, I live in the east coast.`);
 }
 
 // console.log(questionTwo);
@@ -31,8 +36,10 @@ if(questionTwo === 'y' || questionTwo === 'yes') {
 let questionThree = prompt('Third Question. Do I enjoy spicy food?').toLocaleLowerCase();
 
 if(questionThree === 'y' || questionThree === 'yes') {
+  //console.log(`Yes! I love spicy food. Awesome guess ${userName}`);
   alert(`Yes! I love spicy food. Awesome guess ${userName}`);
 } else if (questionThree === 'n' || questionThree === 'no') {
+// console.log(`Sadly, that is incorrect ${userName}. I need spice in my life!`);
   alert(`Sadly, that is incorrect ${userName}. I need spice in my life!`);
 }
 
@@ -42,8 +49,10 @@ if(questionThree === 'y' || questionThree === 'yes') {
 let questionFour = prompt('Fourth Question. Did I play any instruments growing up?').toLocaleLowerCase();
 
 if(questionFour === 'y' || questionFour === 'yes') {
+  //console.log(`Yes! Great guess ${userName}. I played the piano, saxophone, and the recorder growing up.`);
   alert(`Yes! Great guess ${userName}. I played the piano, saxophone, and the recorder growing up.`);
 } else if (questionFour === 'n' || questionFour === 'no') {
+// console.log(`Sadly, that is incorrect ${userName}. I played several instruments growing up!`);
   alert(`Sadly, that is incorrect ${userName}. I played several instruments growing up!`);
 }
 
@@ -53,8 +62,10 @@ if(questionFour === 'y' || questionFour === 'yes') {
 let questionFive = prompt('Final Question! Do I have any siblings?').toLocaleLowerCase();
 
 if(questionFive === 'y' || questionFive === 'yes') {
-  alert(`You are great at this guessing game ${userName}! I have one older brother. hank you so much for answering these 5 questions, now welcome to my page!`);
+  //console.log(`You are great at this guessing game ${userName}! I have one older brother. Thank you so much for answering these 5 questions, now welcome to my page!`);
+  alert(`You are great at this guessing game ${userName}! I have one older brother. Thank you so much for answering these 5 questions, now welcome to my page!`);
 } else if (questionFive === 'n' || questionFive === 'no') {
+  //console.log(`Sadly, that is incorrect ${userName}. I have one older sibling. Thank you so much for answering these 5 questions, now welcome to my page!`);
   alert(`Sadly, that is incorrect ${userName}. I have one older sibling. Thank you so much for answering these 5 questions, now welcome to my page!`);
 }
 
